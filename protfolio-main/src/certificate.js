10b1bbb8e83d18261478c8382de8ebceca3b7bea
Shivkumargowdru <kshivakumar7227@gmail.com>
2022-03-12 21:13:01 +0530

    Code committed

---
+++ protfolio-main/src/certificate.js
@@ -0,0 +1,31 @@
+import React from "react";
+import toolkit from './images/toolkit.jpeg';
+import js from './images/js.png';
+import fullstack from './images/fullstack.jpeg';
+import sih from './images/SIH.jpg';
+
+function Certificate() {
+    return (
+        <>
+            <div>
+                < section className="colorlib-experience" data-section="certificate">
+                    <div className="colorlib-narrow-content">
+                       
+                       
+                        <div className="row">
+                            
+                        </div>
+                        <div className="row">
+                            
+                            
+                            
+                           
+                        </div>
+                    </div>
+                </ section>
+            </div>
+        </>
+    );
+}
+
+export default Certificate;
